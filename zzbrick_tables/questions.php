5ccaa2ed8f59951c02bb9fdd5c4ab5f88600ab7c
Gustaf Mossakowski <gustaf@koenige.org>
2023-06-03 12:52:15 +0200

    questions: show answers if available

--- zzbrick_tables/questions.php
+++ zzbrick_tables/questions.php
@@ -27,6 +27,23 @@ $zz['fields'][3]['type'] = 'select';
 $zz['fields'][3]['enum'] = ['yes', 'no'];
 $zz['fields'][3]['default'] = 'no';
 
+$zz['fields'][4]['field_name'] = 'answers';
+$zz['fields'][4]['type'] = 'subtable';
+$zz['fields'][4]['table'] = 'votes';
+$zz['fields'][4]['hide_in_form'] = true;
+$zz['fields'][4]['fields'][1]['field_name'] = 'vote_id';
+$zz['fields'][4]['fields'][1]['type'] = 'id';
+$zz['fields'][4]['fields'][2]['field_name'] = 'question_id';
+$zz['fields'][4]['fields'][2]['type'] = 'foreign_key';
+$zz['fields'][4]['sql'] = 'SELECT * FROM votes
+	LEFT JOIN questions USING (question_id)';
+$zz['fields'][4]['subselect']['sql'] = 'SELECT question_id, CONCAT(answer, ": ", COUNT(*))
+	FROM votes
+	LEFT JOIN questions USING (question_id)
+	GROUP BY question_id, answer
+	ORDER BY answer';
+$zz['fields'][4]['subselect']['concat_rows'] = ' – ';
+
 $zz['sql'] = 'SELECT /*_PREFIX_*/questions.*
 	FROM /*_PREFIX_*/questions';
 $zz['sqlorder'] = ' ORDER BY question_id';
